5cd10ec0367e849661558f58845edf83bd574933
Travis CI <builds@travis-ci.com>
2017-12-30 19:11:23 +0100

    #2 create LimitedExecutor

--- src/test/java/com/github/adeynack/executors/LimitedExecutorTest.java
+++ src/test/java/com/github/adeynack/executors/LimitedExecutorTest.java
@@ -5,7 +5,9 @@ import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
@@ -17,6 +19,8 @@ import java.util.stream.IntStream;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
@@ -24,19 +28,25 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 @SuppressWarnings("Duplicates") // tolerated for test code
-public class SequencedExecutorTest {
+public class LimitedExecutorTest {
 
-  private SequencedExecutor sequencedExecutor;
+  private LimitedExecutor limitedExecutor;
   private SequentialChecker sequentialChecker;
   private AtomicInteger createdTaskCounter;
   private CompletableFuture<Integer> startedTaskWait;
+
+  private final Field fieldTasksSubmitted = LimitedExecutor.class.getDeclaredField("tasksSubmitted");
+  private final Field fieldTaskQueue = LimitedExecutor.class.getDeclaredField("taskQueue");
+
+  public LimitedExecutorTest() throws NoSuchFieldException {
+  }
 
   @Before
   public void before() {
     final Executor baseExecutor = Executors.newFixedThreadPool(
         4,
-        new ThreadFactoryBuilder().setNameFormat("sequenced-executor-test-pool-%d").build());
-    sequencedExecutor = new SequencedExecutor(baseExecutor);
+        new ThreadFactoryBuilder().setNameFormat("limited-executor-test-pool-%d").build());
+    limitedExecutor = new LimitedExecutor(baseExecutor, 3);
     sequentialChecker = new SequentialChecker();
     createdTaskCounter = new AtomicInteger();
     startedTaskWait = new CompletableFuture<>();
@@ -49,13 +59,13 @@ public class SequencedExecutorTest {
       try {
         sequentialChecker.check(taskId.toString(), true, () -> {
           if (rePost) {
-            sequencedExecutor.execute(createTask(taskId + 100, false, false));
+            limitedExecutor.execute(createTask(taskId + 100, false, false));
           }
           if (fail) {
             throw new RuntimeException(String.format("Task %s fails", taskId));
           }
           try {
-            Thread.sleep(100);
+            Thread.sleep(200);
           } catch (InterruptedException e) {
             throw new RuntimeException(e);
           }
@@ -71,35 +81,39 @@ public class SequencedExecutorTest {
     };
   }
 
-  private void assertAllTasksExecutedOnSequencedExecutor() {
+  private void assertAllTasksExecutedOnLimitedExecutor() {
     sequentialChecker.getLog().forEach((entry) -> {
-      assertThat(entry.threadName, startsWith("sequenced-executor-test-pool-"));
+      assertThat(entry.threadName, startsWith("limited-executor-test-pool-"));
     });
   }
 
   @Test
-  public void twoTasksAreNotExecutedAtTheSameTime() throws Exception {
-    IntStream.range(0, 12)
-             .forEach(i -> sequencedExecutor.execute(createTask(i, false, false)));
+  public void notMoreThan3TasksAreExecutedAtTheSameTime() throws Exception {
+    IntStream.range(0, 24)
+             .forEach(i -> limitedExecutor.execute(createTask(i, false, false)));
     startedTaskWait.get(1, TimeUnit.MINUTES);
-    sequentialChecker.assertMaxParallelTaskCount(1);
-    assertAllTasksExecutedOnSequencedExecutor();
+    sequentialChecker.assertMaxParallelTaskCount(3);
+    assertAllTasksExecutedOnLimitedExecutor();
+    assertThat(limitedExecutor.getTasksSubmitted(), equalTo(0));
+    assertThat(limitedExecutor.getTaskQueue(), empty());
   }
 
   @Test
-  public void twoTasksAreNotExecutedAtTheSameTimeWhenSubmittedFromAnExecutedTask() throws Exception {
-    IntStream.range(0, 12)
-             .forEach(i -> sequencedExecutor.execute(createTask(i, true, false)));
+  public void notMoreThan3TasksAreExecutedAtTheSameTimeWhenSubmittedFromAnExecutedTask() throws Exception {
+    IntStream.range(0, 24)
+             .forEach(i -> limitedExecutor.execute(createTask(i, true, false)));
     startedTaskWait.get(1, TimeUnit.MINUTES);
-    sequentialChecker.assertMaxParallelTaskCount(1);
-    assertAllTasksExecutedOnSequencedExecutor();
+    sequentialChecker.assertMaxParallelTaskCount(3);
+    assertAllTasksExecutedOnLimitedExecutor();
+    assertThat(limitedExecutor.getTasksSubmitted(), equalTo(0));
+    assertThat(limitedExecutor.getTaskQueue(), empty());
   }
 
   @Test
   public void aFailingTaskDoesNotCrashTheExecutor() throws Exception {
     final List<CompletableFuture<Void>> futures =
-        IntStream.range(0, 12)
-                 .mapToObj(i -> CompletableFuture.runAsync(createTask(i, false, i == 5), sequencedExecutor))
+        IntStream.range(0, 24)
+                 .mapToObj(i -> CompletableFuture.runAsync(createTask(i, false, i == 5), limitedExecutor))
                  .collect(Collectors.toList());
 
     // Expecting the execution to fail.
@@ -113,55 +127,42 @@ public class SequencedExecutorTest {
       assertEquals("Task 5 fails", cause.getMessage());
     }
 
-    sequentialChecker.assertMaxParallelTaskCount(1);
+    sequentialChecker.assertMaxParallelTaskCount(3);
 
     // future at index 5 should have failed.
     assertTrue(futures.get(5).isCompletedExceptionally());
     // all others should have not
-    assertFalse(futures.get(0).isCompletedExceptionally());
-    assertFalse(futures.get(1).isCompletedExceptionally());
-    assertFalse(futures.get(2).isCompletedExceptionally());
-    assertFalse(futures.get(3).isCompletedExceptionally());
-    assertFalse(futures.get(4).isCompletedExceptionally());
-    assertFalse(futures.get(6).isCompletedExceptionally());
-    assertFalse(futures.get(7).isCompletedExceptionally());
-    assertFalse(futures.get(8).isCompletedExceptionally());
-    assertFalse(futures.get(9).isCompletedExceptionally());
-    assertFalse(futures.get(10).isCompletedExceptionally());
-    assertFalse(futures.get(11).isCompletedExceptionally());
+    IntStream.range(0, 24)
+             .filter(i -> i != 5)
+             .forEach(i -> assertFalse(futures.get(i).isCompletedExceptionally()));
 
-    assertAllTasksExecutedOnSequencedExecutor();
+    assertAllTasksExecutedOnLimitedExecutor();
+    assertThat(limitedExecutor.getTasksSubmitted(), equalTo(0));
+    assertThat(limitedExecutor.getTaskQueue(), empty());
   }
 
   @Test
   public void whenUsedInAFutureItReturnsTheValue() throws Exception {
     final List<CompletableFuture<Integer>> futures =
-        IntStream.range(0, 12)
+        IntStream.range(0, 24)
                  .mapToObj(i -> CompletableFuture.supplyAsync(() -> {
                    createTask(i, false, false).run();
                    return i;
-                 }, sequencedExecutor))
+                 }, limitedExecutor))
                  .collect(Collectors.toList());
 
     CompletableFuture.allOf(futures.toArray(new CompletableFuture<?>[futures.size()]))
                      .get(1, TimeUnit.MINUTES);
 
-    sequentialChecker.assertMaxParallelTaskCount(1);
+    sequentialChecker.assertMaxParallelTaskCount(3);
 
-    assertEquals(0, futures.get(0).get().intValue());
-    assertEquals(1, futures.get(1).get().intValue());
-    assertEquals(2, futures.get(2).get().intValue());
-    assertEquals(3, futures.get(3).get().intValue());
-    assertEquals(4, futures.get(4).get().intValue());
-    assertEquals(5, futures.get(5).get().intValue());
-    assertEquals(6, futures.get(6).get().intValue());
-    assertEquals(7, futures.get(7).get().intValue());
-    assertEquals(8, futures.get(8).get().intValue());
-    assertEquals(9, futures.get(9).get().intValue());
-    assertEquals(10, futures.get(10).get().intValue());
-    assertEquals(11, futures.get(11).get().intValue());
+    for (int i = 0; i < 24; ++i) {
+      assertEquals(i, futures.get(i).get().intValue());
+    }
 
-    assertAllTasksExecutedOnSequencedExecutor();
+    assertAllTasksExecutedOnLimitedExecutor();
+    assertThat(limitedExecutor.getTasksSubmitted(), equalTo(0));
+    assertThat(limitedExecutor.getTaskQueue(), empty());
   }
 
 }
